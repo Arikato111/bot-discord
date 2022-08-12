e338b834ad52dfa3188f632efd5606253146b1d7
Nawasan <arikato110011@gmail.com>
2022-08-13 00:43:55 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Bot for Discord
+---
+## Get started 
+1. create file .env and set `token` from Discord bot
+    ```
+    token=<your-token>
+    ```
+2. run `npm install` to install packages
+3. run `npm run dev` to start this bot
+---
---
+++ bot.js
@@ -0,0 +1,56 @@
+const Discord = require("discord.js");
+require("dotenv").config();
+const client = new Discord.Client({
+  intents: ["Guilds", "GuildMessages", "MessageContent"],
+});
+client.on("ready", () => {
+  console.log(`Loged in as ${client.user.tag}`);
+});
+
+client.on("messageCreate", (msg) => {
+  if (!msg.author.bot) {
+    if (msg.content.toLocaleLowerCase() === "hello") {
+      msg.reply(`Hello ${msg.author.username}, Have a good day~`);
+      return;
+    }
+    if (msg.content === "สวัสดี") {
+      msg.reply(`สวัสดี~ ${msg.author.username}`);
+      return;
+    }
+    if (msg.content.toLocaleLowerCase() === "maria") {
+      msg.reply("I am here.");
+      return;
+    }
+    if (msg.content.toLocaleLowerCase() === "what's time") {
+      msg.reply(Date());
+      return;
+    }
+    if (msg.content === "こんにちは") {
+      msg.reply("こにちは、お疲れさまでした。");
+      return;
+    }
+    if (msg.content.toLocaleLowerCase() === "img") {
+      msg.channel.send(
+        "はい~" +
+          "https://www.jnto.or.th/wp-content/uploads/2020/01/best-hayazaki-sakura-spot-10.jpg"
+      );
+      return;
+    }
+
+    var command = msg.content.split(" ");
+    if (command.length > 1 && command[0].toLocaleLowerCase() === "maria") {
+      switch (command[1].toLocaleLowerCase()) {
+        case "github":
+          msg.reply("https://github.com/Arikato111/bot-discord");
+          return;
+        case "time":
+          msg.reply(
+            new Date().toLocaleString('en-US')
+          );
+          return;
+      }
+    }
+  }
+});
+
+client.login(process.env.token);
